90dbac6c3c845e7378c33cd896d06d99e4c8bbc4
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-07 16:07:14 +0200

    First commit

---
+++ download-assets.sh
@@ -0,0 +1,5 @@
+curl -sL https://cdn.jsdelivr.net/npm/alpinejs@3/dist/cdn.min.js > static/cdn.min.js
+curl -sL https://cdn.jsdelivr.net/npm/jszip@3/dist/jszip.min.js > static/jszip.min.js
+curl -sL https://cdn.jsdelivr.net/npm/sql.js@1/dist/sql-wasm.js > static/sql-wasm.js
+curl -sL https://cdn.jsdelivr.net/npm/sql.js@1/dist/sql-wasm.wasm > static/sql-wasm.wasm
+curl -sL https://cdn.jsdelivr.net/npm/tablesort@5/dist/tablesort.min.js > static/tablesort.min.js
