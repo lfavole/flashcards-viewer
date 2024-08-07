ae310f28294ff9aa1f7fc7b67469c0d73912b30d
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-07 17:16:13 +0200

    Add translations

---
+++ static/fr.js
@@ -0,0 +1,8 @@
+translations.fr = {
+    "Flashcards viewer": "Visualiseur de flashcards",
+    "Select file to open:": "Sélectionner un fichier à ouvrir :",
+    "Sort field": "Champ de tri",
+    "Question": "Question",
+    "Answer": "Réponse",
+    "Flip": "Retourner",
+};
