9300cd8ced73b5cbd0679475044a1a9320f05e27
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-09 13:11:37 +0200

    feat: Add dropzone and collapsible decks

--- static/fr.js
+++ static/fr.js
@@ -1,6 +1,9 @@
 translations.fr = {
     "Flashcards viewer": "Visualiseur de flashcards",
     "Select file to open:": "Sélectionner un fichier à ouvrir :",
+    "Release the file!": "Relâchez le fichier !",
+    "Release the files!": "Relâchez les fichiers !",
+    "Or drag some files into this window": "Ou déposez des fichiers dans cette fenêtre",
     "Sort field": "Champ de tri",
     "Question": "Question",
     "Answer": "Réponse",
