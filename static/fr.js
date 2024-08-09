223080471363940a3aaaa9b3789c593d0315c2af
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-09 14:22:40 +0200

    feat: Add a field to load a file via URL

--- static/fr.js
+++ static/fr.js
@@ -4,6 +4,8 @@ translations.fr = {
     "Release the file!": "Relâchez le fichier !",
     "Release the files!": "Relâchez les fichiers !",
     "Or drag some files into this window": "Ou déposez des fichiers dans cette fenêtre",
+    "You can also load a file with its URL:": "Vous pouvez aussi charger un fichier avec son URL:",
+    "Load": "Charger",
     "Sort field": "Champ de tri",
     "Question": "Question",
     "Answer": "Réponse",
