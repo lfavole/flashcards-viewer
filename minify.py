179cdc95f409227c5ca9578b076295d0f9b5a125
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-10 13:22:21 +0200

    perf: Minify JavaScript attributes (Alpine.js)

--- minify.py
+++ minify.py
@@ -1,6 +1,32 @@
 from pathlib import Path
+import re
 
 import minify_html
+
+
+def minify_with_prefix(code, prefix, file: str | Path = "snippet"):
+    try:
+        return minify_html.minify(
+            prefix + code,
+            do_not_minify_doctype=True,
+            minify_css=True,
+            minify_js=True,
+        ).removeprefix(prefix)
+    except BaseException as exc:
+        print(
+            f"::warning title=Minification of {file} failed"
+            + (", skipping file" if file != "snippet" else "")
+            + f"::{type(exc).__qualname__}: {exc}"
+        )
+        return code
+
+
+def minify_js(js):
+    return sorted(
+        [minify_with_prefix(js, "<script>"), minify_with_prefix(js, "<script>x=")],
+        key=len,
+    )[0]
+
 
 files_to_edit = [*Path("site/static").glob("**/*"), Path("site/index.html")]
 
@@ -14,19 +40,17 @@ for file in files_to_edit:
         print(f"Minifying {file}")
 
         data = file.read_text("utf-8")
-        try:
-            data = minify_html.minify(
-                prefix + data,
-                do_not_minify_doctype=True,
-                minify_css=True,
-                minify_js=True,
-            ).removeprefix(prefix)
-        except BaseException as exc:
-            print(
-                f"::warning title=Minification of {file} failed, skipping file::{type(exc).__qualname__}: {exc}"
+        if file.suffix == ".html":
+            # Minify JavaScript attributes (Alpine.js)
+            data = re.sub(
+                r"=([\"'])(.*?)\1",
+                lambda match: "=" + match[1] + minify_js(match[2]) + match[1],
+                data,
             )
-        else:
-            file.write_text(data, "utf-8")
+
+        data = minify_with_prefix(data, prefix, file)
+
+        file.write_text(data, "utf-8")
 
     else:
         print(f"Skipping minification of {file}, no minifier available")
