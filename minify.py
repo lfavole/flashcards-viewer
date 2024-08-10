0329a8559ebb365a58a05bcc61cb781895d06d52
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-10 12:24:21 +0200

    refactor: Use minify-html package for minification

--- minify.py
+++ minify.py
@@ -1,5 +1,6 @@
-import subprocess as sp
 from pathlib import Path
+
+import minify_html
 
 files_to_edit = [*Path("site/static").glob("**/*"), Path("site/index.html")]
 
@@ -7,39 +8,25 @@ for file in files_to_edit:
     if file.is_dir():
         continue
 
-    if file.suffix == ".html":
-        print(f"Minifying {file} with html-minifier")
-        sp.run(
-            [
-                "html-minifier",
-                "--collapse-boolean-attributes",
-                "--collapse-whitespace",
-                "--decode-entities",
-                "--minify-css",
-                "true",
-                "--minify-js",
-                "true",
-                "--no-include-auto-generated-tags",
-                "--remove-comments",
-                "--remove-optional-tags",
-                "--remove-redundant-attributes",
-                "--remove-script-type-attributes",
-                "--remove-style-link-type-attributes",
-                "--remove-tag-whitespace",
-                str(file),
-                "--output",
-                str(file),
-            ],
-            check=True,
-        )
+    prefix = {".html": "", ".css": "<style>", ".js": "<script>"}.get(file.suffix)
 
-    elif file.suffix == ".css":
-        print(f"Minifying {file} with csso")
-        sp.run(["csso", str(file), "--source-map", "file", "--output", str(file)], check=True)
+    if prefix is not None:
+        print(f"Minifying {file}")
 
-    elif file.suffix == ".js":
-        print(f"Minifying {file} with uglifyjs")
-        sp.run(["uglifyjs", str(file), "--source-map", "--output", str(file)], check=True)
+        data = file.read_text("utf-8")
+        try:
+            data = minify_html.minify(
+                prefix + data,
+                do_not_minify_doctype=True,
+                minify_css=True,
+                minify_js=True,
+            ).removeprefix(prefix)
+        except BaseException as exc:
+            print(
+                f"::warning title=Minification of {file} failed, skipping file::{type(exc).__qualname__}: {exc}"
+            )
+        else:
+            file.write_text(data, "utf-8")
 
     else:
         print(f"Skipping minification of {file}, no minifier available")
