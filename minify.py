e11b82e2bec483b122c6517dbe615513678fe093
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-08-10 13:55:14 +0200

    perf: Use UglifyJS for JavaScript minification

--- minify.py
+++ minify.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import re
+import subprocess as sp
 
 import minify_html
 
@@ -22,19 +23,36 @@ def minify_with_prefix(code, prefix, file: str | Path = "snippet"):
 
 
 def minify_js(js):
-    if js.startswith("await "):
-        return "await " + minify_with_prefix(js.removeprefix("await "), "<script>x=")
-
-    return sorted(
-        [minify_with_prefix(js, "<script>"), minify_with_prefix(js, "<script>x=")],
-        key=len,
-    )[0]
+    try:
+        return (
+            sp.run(
+                ["uglifyjs", "--beautify", "beautify=false", "quote_style=3"],
+                stdout=sp.PIPE,
+                input=js,
+                text=True,
+                encoding="utf-8",
+                check=True,
+            )
+            .stdout.rstrip()
+            .rstrip(";")
+        )
+    except sp.CalledProcessError as exc:
+        print(f"::warning title=Minification of snippet failed::{type(exc).__qualname__}: {exc}")
+        return js
 
 
 files_to_edit = [*Path("site/static").glob("**/*"), Path("site/index.html")]
 
 for file in files_to_edit:
     if file.is_dir():
+        continue
+
+    if file.suffix == ".js":
+        print(f"Minifying {file} with UglifyJS")
+        try:
+            sp.run(["uglifyjs", str(file), "--source-map", "--output", str(file)], check=True)
+        except sp.CalledProcessError as exc:
+            print(f"::warning title=Minification of {file} failed, skipping file::{type(exc).__qualname__}: {exc}")
         continue
 
     prefix = {".html": "", ".css": "<style>", ".js": "<script>"}.get(file.suffix)
