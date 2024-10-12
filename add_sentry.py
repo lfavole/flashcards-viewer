b79162bc15d172e16b0f7c8e41a7018a90893e43
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-10-12 10:36:20 +0200

    fix: Don't add Sentry script in <head>

--- add_sentry.py
+++ add_sentry.py
@@ -16,14 +16,6 @@ sentry_data = sentry_file.read_text("utf-8")
 
 SENTRY_DSN = os.environ.get("SENTRY_DSN", "")
 if SENTRY_DSN:
-    # Add the script in the <head> tag
-    print("Adding Sentry script in head")
-    index_data = index_data.replace(
-        "</head>",
-        '    <script src="static/sentry.js" async></script></script>\n</head>',
-    )
-    index_file.write_text(index_data, "utf-8")
-
     # Add the Sentry DSN in the Sentry script
     print("Adding Sentry DSN in script")
     sentry_data = sentry_data.replace('SENTRY_DSN = ""', f'SENTRY_DSN = "{SENTRY_DSN}"')
